5664c951e2e6f82860637c28e3809f9682b911ec
lucas <lucasvargasdev05@gmail.com>
2025-04-18 16:12:59 -0300

    fix: Update homepage URL to use HTTPS; correct stylesheet path and import Rotas component with .jsx extension

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-import Rotas from "./Rotas";
+import Rotas from "./Rotas.jsx";
 
 const contentor = document.getElementById("root");
 const origem = createRoot(contentor);
